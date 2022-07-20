77f3b440575b1153a7206cbea5b21fe4514f7d51
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-20 20:41:13 -0300

    fix(task.controller): wrong parameters on methods

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,35 +1,35 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { TaskService } from './task.service';
-import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Prisma } from '@prisma/client';
 
 @Controller()
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @MessagePattern('createTask')
-  create(@Payload() createTaskDto: CreateTaskDto) {
-    return this.taskService.create(createTaskDto);
+  async create(@Payload() data: Prisma.TaskCreateInput) {
+    return this.taskService.create(data);
   }
 
   @MessagePattern('findAllTask')
-  findAll() {
-    return this.taskService.findAll();
+  async findAll(@Payload() where: Prisma.TaskWhereInput) {
+    return this.taskService.findAll(where);
   }
 
   @MessagePattern('findOneTask')
-  findOne(@Payload() id: number) {
+  async findOne(@Payload() id: string) {
     return this.taskService.findOne(id);
   }
 
   @MessagePattern('updateTask')
-  update(@Payload() updateTaskDto: UpdateTaskDto) {
+  async update(@Payload() updateTaskDto: UpdateTaskDto) {
     return this.taskService.update(updateTaskDto.id, updateTaskDto);
   }
 
   @MessagePattern('removeTask')
-  remove(@Payload() id: number) {
+  async remove(@Payload() id: string) {
     return this.taskService.remove(id);
   }
 }
