b9a52d45f7843b554b5577ab3bfa4db03bb831b1
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-21 21:02:52 -0300

    text(task.controller): create valid task

--- src/task/task.controller.spec.ts
+++ src/task/task.controller.spec.ts
@@ -1,20 +1,44 @@
+import { PrismaService } from '@/prisma/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { v4 as uuid4 } from 'uuid';
+import { ConfigModule } from '@nestjs/config';
 
 describe('TaskController', () => {
   let controller: TaskController;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: [`.env.${process.env.NODE_ENV}`],
+        }),
+      ],
       controllers: [TaskController],
-      providers: [TaskService],
+      providers: [TaskService, PrismaService],
     }).compile();
 
     controller = module.get<TaskController>(TaskController);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('valid task', async () => {
+      const task = {
+        name: 'Test task',
+        description: 'Testing creation',
+        userId: uuid4(),
+      };
+      const createdTask = await controller.create(task);
+      expect(createdTask).toMatchObject(task);
+    });
+
+    it('invalid task', async () => { });
   });
 });
