d9e3e6731cfdc50563536dd0f643d91c42762e96
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-23 14:53:45 -0300

    feat: configuring rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      option: {
+        url: [process.env.CLOUDAMQP_URL],
+        queu: 'tasks_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
