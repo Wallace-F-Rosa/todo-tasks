ba9d647105689d9f3dc5153612150608765ceb11
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-23 16:56:54 -0300

    test(task.controller): completing crud testing

--- src/task/task.controller.spec.ts
+++ src/task/task.controller.spec.ts
@@ -7,8 +7,9 @@ import { ConfigModule } from '@nestjs/config';
 
 describe('TaskController', () => {
   let controller: TaskController;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({
@@ -20,21 +21,85 @@ describe('TaskController', () => {
     }).compile();
 
     controller = module.get<TaskController>(TaskController);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  beforeEach(async () => {
+    await prisma.task.deleteMany({});
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('create', () => {
-    it('valid task', async () => {
-      const task = {
-        name: 'Test task',
-        description: 'Testing creation',
-        userId: uuid4(),
-      };
-      const createdTask = await controller.create(task);
-      expect(createdTask).toMatchObject(task);
-    });
+  it('valid task', async () => {
+    const task = {
+      name: 'Test task',
+      description: 'Testing creation',
+      userId: uuid4(),
+    };
+    const createdTask = await controller.create(task);
+    expect(createdTask).toMatchObject(task);
+  });
+
+  it('update task', async () => {
+    const task = {
+      name: 'Test task',
+      description: 'Testing creation',
+      userId: uuid4(),
+    };
+    const createdTask = await prisma.task.create({ data: task });
+    const updateData = {
+      id: createdTask.id,
+      name: 'Updated task',
+      description: 'Updated text',
+    };
+    const updatedTask = await controller.update(updateData);
+    createdTask.name = updateData.name;
+    createdTask.description = updateData.description;
+    expect(updatedTask).toMatchObject(createdTask);
+  });
+
+  it('delete task', async () => {
+    const task = {
+      name: 'Test task',
+      description: 'Testing creation',
+      userId: uuid4(),
+    };
+    const createdTask = await prisma.task.create({ data: task });
+    const deletedTask = await controller.remove(createdTask.id);
+    expect(deletedTask).toMatchObject(createdTask);
+  });
+
+  it('list tasks', async () => {
+    const user1 = uuid4();
+    const user2 = uuid4();
+    const tasks = [
+      {
+        name: 'task1',
+        description: 'first task',
+        userId: user1,
+      },
+      {
+        name: 'task2',
+        description: 'second task',
+        userId: user1,
+      },
+      {
+        name: 'task3',
+        description: 'third task',
+        userId: user2,
+      },
+    ];
+    const createdTasks = [];
+    for (const t of tasks) {
+      createdTasks.push(await prisma.task.create({ data: t }));
+    }
+    const allTasks = await controller.findAll({});
+    expect(allTasks).toMatchObject(expect.arrayContaining(createdTasks));
+
+    const userTasks = createdTasks.filter((item) => item.userId === user1);
+    const userTaskList = await controller.findAll({ userId: user1 });
+    expect(userTaskList).toMatchObject(expect.arrayContaining(userTasks));
   });
 });
