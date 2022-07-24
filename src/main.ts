043515d29389bdac835cb09d8413be2584fd5efb
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-24 17:12:27 -0300

    fix(main): Transport.RMQ not assignable to Transport.KAFKA
    can't define the app port, resulting in error

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3001;
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.RMQ,
       options: {
-        port,
         urls: [process.env.CLOUDAMQP_URL || 'amqp://localhost:5672'],
         queue: 'tasks_queue',
         queueOptions: {
