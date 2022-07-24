adb2f478ab7e1168352d19b9b6e183e3976fd3a9
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-24 18:25:53 -0300

    feat: going back to previous app bootstrap config

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.CLOUDAMQP_URL || 'amqp://localhost:5672'],
-      queue: 'tasks_queue',
-      queueOptions: {
-        durable: false,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.CLOUDAMQP_URL || 'amqp://localhost:5672'],
+        queue: 'tasks_queue',
+        queueOptions: {
+          durable: false,
+        },
       },
     },
-  });
-  await app.listen(process.env.PORT || 3001);
+  );
+  await app.listen();
 }
 bootstrap();
