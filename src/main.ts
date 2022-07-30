cf43607a43599fac12cce7f05ccb70323a4cc0a0
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-30 14:24:44 -0300

    feat(main): serve favicon

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('public');
   app.connectMicroservice<MicroserviceOptions>(
     {
       transport: Transport.RMQ,
